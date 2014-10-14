f1be9944f8d61ffde329239d932c20e4d3dae783
Tom Jenkins <tom@itsravenous.com>
2014-10-14 15:03:00 +0100

    Adding class skeleton

---
+++ src/compare.js
@@ -0,0 +1,32 @@
+/**
+ * @file Class to compare fingerprints
+ * @author Tom Jenkins tom@itsravenous.com
+ */
+
+// Dependencies
+var Base = require('basejs');
+
+var Compare = Base.extend({
+	
+	/**
+	 * @constructor Compare
+	 */
+	constructor: function () {
+
+	},
+
+	/**
+	 * @memberof Compare
+	 */
+	compareTwo: function () {
+
+	},
+
+	/**
+	 * @memberof Compare
+	 */
+	exhaustiveSearch: function () {
+		
+	}
+
+});
