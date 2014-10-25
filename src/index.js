bfaf68f31306f7bcc9dc19b7d98067f884c6f19d
Tom Jenkins <tom@itsravenous.com>
2014-10-25 12:12:46 +0100

    Adding missing entry point file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * @file Entry point for library
+ * @author Tom Jenkins tom@itsravenous.com
+ */
+
+module.exports = {
+	Compare: require('./compare'),
+	FingerPrint: require('./fingerprint'),
+	Element: require('./element')
+};
